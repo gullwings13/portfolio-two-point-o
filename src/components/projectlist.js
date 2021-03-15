678755d5ea4e91910afa90b8f21f03e287d44ca4
Trevor Smith-Holbourn <dr4g0n@gmail.com>
2021-03-14 21:53:11 -0400

    reordered projects

--- src/components/projectlist.js
+++ src/components/projectlist.js
@@ -18,13 +18,58 @@ export const projects = [
         livesite: "https://play.google.com/store/apps/details?id=com.ehsani.drivingapp&hl=en_US&gl=US"
     },
     {
-        title: "The X-Effect",
-        image: "xeffect.png",
-        description: "Motivational and productivity habit building system, built by team of two. Backend built using Node, Express and MongoDB.",
-        tags: ["react", "mongodb", "tailwindcss", "express"],
+        title: "Orbital Simulation",
+        image: "orbital.png",
+        description: "Orbital simulation toy using Unity DOTS/ECS and Unity cloud build. Supports hundreds of thousands of orbiting bodies.",
+        tags: ["unity", "ECS/DOTS", "toy"],
+        filters: ["unity/c#"],
+        github: "https://github.com/gullwings13/legendary-octo-guacamole",
+        livesite: "https://tsh.itch.io/orbital-simulation"
+    },
+    {
+        title: "Polymino Gardens",
+        image: "polyminogardens.png",
+        description: "Tile base pattern matching puzzle game with an animated UI, object pooling and procedural gardens.",
+        tags: ["unity", "procedural", "game"],
+        filters: ["unity/c#"],
+        github: null,
+        livesite: "https://tsh.itch.io/polyminogardens"
+    },
+    {
+        title: "VR Prototype",
+        image: "vrprototype.png",
+        description: "This VR prototype explores different firing mechanics. Incorporating steam VR API.",
+        tags: ["unity", "vr", "game"],
+        filters: ["unity/c#"],
+        github: null,
+        livesite: "https://tsh.itch.io/vrprototype"
+    },
+    {
+        title: "Space Jump Fun",
+        image: "spacejumpfun.png",
+        description: "A casual 2d platformer using c# interfaces and floaty jumping physics.",
+        tags: ["unity", "interfaces", "game"],
+        filters: ["unity/c#"],
+        github: null,
+        livesite: "https://tsh.itch.io/space-jump-fun"
+    },
+    {
+        title: "Witch Sword",
+        image: "witchsword.png",
+        description: "Action RPG exploring VR elements and platforming game-play.",
+        tags: ["unity", "vr", "ai", "game"],
+        filters: ["unity/c#"],
+        github: null,
+        livesite: "https://tsh.itch.io/witchswordv2"
+    },
+    {
+        title: "Brood Battle",
+        image: "broodbattle.png",
+        description: "Realtime multiplayer, multi-room quiz game using socket.io. Tested with artillery.io.",
+        tags: ["react", "javascript", "tailwindcss", "expressjs", "socket.io"],
         filters: ["fullstack"],
-        github: "https://github.com/Khimarra/the-x-effect",
-        livesite: "https://thexeffect.netlify.app/"
+        github: null,
+        livesite: "https://brood-battle.netlify.app/"
     },
     {
         title: "ParkSafe",
@@ -45,69 +90,6 @@ export const projects = [
         livesite: "https://fetch-hs.netlify.app/"
     },
     {
-        title: "Brood Battle",
-        image: "broodbattle.png",
-        description: "Realtime multiplayer, multi-room quiz game using socket.io. Tested with artillery.io.",
-        tags: ["react", "javascript", "tailwindcss", "expressjs", "socket.io"],
-        filters: ["fullstack"],
-        github: null,
-        livesite: "https://brood-battle.netlify.app/"
-    },
-    {
-        title: "Paprika.Studio",
-        image: "paprikastudio.png",
-        description: "Custom Gatsby static markdown blog with automatic dark-theme CSS.",
-        tags: ["gatsby", "react", "javascript"],
-        filters: ["frontend"],
-        github: null,
-        livesite: "https://test-1122334455.netlify.app/"
-    },
-    {
-        title: "Orbital Simulation",
-        image: "orbital.png",
-        description: "Orbital simulation toy using Unity DOTS/ECS and Unity cloud build. Supports hundreds of thousands of orbiting bodies.",
-        tags: ["unity", "ECS/DOTS", "toy"],
-        filters: ["unity/c#"],
-        github: "https://github.com/gullwings13/legendary-octo-guacamole",
-        livesite: "https://tsh.itch.io/orbital-simulation"
-    },
-    {
-        title: "Yay!Walker",
-        image: "yaywalker.png",
-        description: "User submitted walking tours. Ruby on Rails backend with React frontend.",
-        tags: ["react", "javascript", "tailwindcss", "rubyonrails"],
-        filters: ["fullstack"],
-        github: "https://github.com/gullwings13/yay-walker",
-        livesite: "https://aqueous-fortress-10362.herokuapp.com/"
-    },
-    {
-        title: "Polymino Gardens",
-        image: "polyminogardens.png",
-        description: "Tile base pattern matching puzzle game with an animated UI, object pooling and procedural gardens.",
-        tags: ["unity", "procedural", "game"],
-        filters: ["unity/c#"],
-        github: null,
-        livesite: "https://tsh.itch.io/polyminogardens"
-    },
-    {
-        title: "Stoc Play",
-        image: "stocplay.png",
-        description: "Trade virtual stock to gain confidence. Built with Javascript, HTML and CSS. Features API request caching and cloudflare worker to proxy API requests.",
-        tags: ["javascript", "css", "restfulapi"],
-        filters: ["frontend"],
-        github: "https://github.com/gullwings13/stoc-play",
-        livesite: "https://gullwings13.github.io/stoc-play/"
-    },
-    {
-        title: "VR Prototype",
-        image: "vrprototype.png",
-        description: "This VR prototype explores different firing mechanics. Incorporating steam VR API.",
-        tags: ["unity", "vr", "game"],
-        filters: ["unity/c#"],
-        github: null,
-        livesite: "https://tsh.itch.io/vrprototype"
-    },
-    {
         title: "Multiverse Trading Game",
         image: "rmtradinggame.png",
         description: "Trade exotic goods while you explore the Multiverse in this React Single Page App.",
@@ -117,14 +99,54 @@ export const projects = [
         livesite: "http://trev-rm-trading-game.surge.sh/"
     },
     {
-        title: "Space Jump Fun",
-        image: "spacejumpfun.png",
-        description: "A casual 2d platformer using c# interfaces and floaty jumping physics.",
-        tags: ["unity", "interfaces", "game"],
-        filters: ["unity/c#"],
-        github: null,
-        livesite: "https://tsh.itch.io/space-jump-fun"
+        title: "Tic Tac Toe",
+        image: "tictactoe.png",
+        description: "Tic tac toe featuring multiplayer and recursive minimax AI algorithm.",
+        tags: ["javascript", "css", "ai"],
+        filters: ["frontend"],
+        github: "https://github.com/gullwings13/tic-tac-toe",
+        livesite: "https://gullwings13.github.io/tic-tac-toe/"
     },
+    {
+        title: "The X-Effect",
+        image: "xeffect.png",
+        description: "Motivational and productivity habit building system, built by team of two. Backend built using Node, Express and MongoDB.",
+        tags: ["react", "mongodb", "tailwindcss", "express"],
+        filters: ["fullstack"],
+        github: "https://github.com/Khimarra/the-x-effect",
+        livesite: "https://thexeffect.netlify.app/"
+    },
+
+    {
+        title: "Paprika.Studio",
+        image: "paprikastudio.png",
+        description: "Custom Gatsby static markdown blog with automatic dark-theme CSS.",
+        tags: ["gatsby", "react", "javascript"],
+        filters: ["frontend"],
+        github: null,
+        livesite: "https://test-1122334455.netlify.app/"
+    },
+
+    // {
+    //     title: "Yay!Walker",
+    //     image: "yaywalker.png",
+    //     description: "User submitted walking tours. Ruby on Rails backend with React frontend.",
+    //     tags: ["react", "javascript", "tailwindcss", "rubyonrails"],
+    //     filters: ["fullstack"],
+    //     github: "https://github.com/gullwings13/yay-walker",
+    //     livesite: "https://aqueous-fortress-10362.herokuapp.com/"
+    // },
+
+    {
+        title: "Stoc Play",
+        image: "stocplay.png",
+        description: "Trade virtual stock to gain confidence. Built with Javascript, HTML and CSS. Features API request caching and cloudflare worker to proxy API requests.",
+        tags: ["javascript", "css", "restfulapi"],
+        filters: ["frontend"],
+        github: "https://github.com/gullwings13/stoc-play",
+        livesite: "https://gullwings13.github.io/stoc-play/"
+    },
+
     {
         title: "NJ Transit Mobile",
         image: "njtransit.png",
@@ -135,15 +157,6 @@ export const projects = [
         livesite: "https://nj-transit-mobile.herokuapp.com/"
     },
     {
-        title: "Witch Sword",
-        image: "witchsword.png",
-        description: "Action RPG exploring VR elements and platforming game-play.",
-        tags: ["unity", "vr", "ai", "game"],
-        filters: ["unity/c#"],
-        github: null,
-        livesite: "https://tsh.itch.io/witchswordv2"
-    },
-    {
         title: "Weather App",
         image: "weather.png",
         description: "Javascript weather app using open weather API with local storage.",
@@ -151,15 +164,6 @@ export const projects = [
         filters: ["frontend"],
         github: "https://github.com/gullwings13/weather-app",
         livesite: "https://gullwings13.github.io/weather-app/"
-    },
-    {
-        title: "Tic Tac Toe",
-        image: "tictactoe.png",
-        description: "Tic tac toe featuring multiplayer and recursive minimax AI algorithm.",
-        tags: ["javascript", "css", "ai"],
-        filters: ["frontend"],
-        github: "https://github.com/gullwings13/tic-tac-toe",
-        livesite: "https://gullwings13.github.io/tic-tac-toe/"
     },
     {
         title: "Portfolio",
